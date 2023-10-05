88bd8460f63cb3e4e2aa49e8f1d26602d595b324
OlegatorLE <oleh.travin@gmail.com>
2023-10-05 14:12:13 +0300

    Complete Tags create, update, delete and list page

--- web_app/urls.py
+++ web_app/urls.py
@@ -5,19 +5,27 @@ from web_app.views import (
     TaskCreateView,
     TaskUpdateView,
     TaskDeleteView,
-    ToggleTaskStatusView
+    ToggleTaskStatusView,
+    TagListView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
 )
 
 urlpatterns = [
     path("", TaskListView.as_view(), name="index"),
-    path('add/', TaskCreateView.as_view(), name='add_task'),
-    path('<int:pk>/edit/', TaskUpdateView.as_view(), name='edit_task'),
-    path('<int:pk>/delete/', TaskDeleteView.as_view(), name='delete_task'),
+    path("add/", TaskCreateView.as_view(), name="add_task"),
+    path("<int:pk>/edit/", TaskUpdateView.as_view(), name="edit_task"),
+    path("<int:pk>/delete/", TaskDeleteView.as_view(), name="delete_task"),
     path(
-        '<int:pk>/toggle/',
+        "<int:pk>/toggle/",
         ToggleTaskStatusView.as_view(),
-        name='toggle_task_status'
+        name="toggle_task_status"
     ),
+    path("tags/", TagListView.as_view(), name="tags"),
+    path("tags/add/", TagCreateView.as_view(), name="add_tag"),
+    path("tags/<int:pk>/edit/", TagUpdateView.as_view(), name="edit_tag"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="delete_tag"),
 ]
 
 app_name = "web_app"
--- web_app/views.py
+++ web_app/views.py
@@ -4,45 +4,77 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 from web_app.forms import TaskForm
-from web_app.models import Task
+from web_app.models import Task, Tag
 
 
 class TaskListView(generic.ListView):
     model = Task
     template_name = "web_app/index.html"
-    context_object_name = 'tasks'
+    context_object_name = "tasks"
 
 
 class TaskCreateView(generic.CreateView):
     model = Task
-    template_name = 'web_app/add_task.html'
+    template_name = "web_app/add_task.html"
     form_class = TaskForm
 
     def get_success_url(self) -> HttpResponse:
-        return reverse_lazy('web_app:index')
+        return reverse_lazy("web_app:index")
 
 
 class TaskUpdateView(generic.UpdateView):
     model = Task
-    template_name = 'web_app/edit_task.html'
+    template_name = "web_app/edit_task.html"
     form_class = TaskForm
 
     def get_success_url(self) -> HttpResponse:
-        return reverse_lazy('web_app:index')
+        return reverse_lazy("web_app:index")
 
 
 class TaskDeleteView(generic.DeleteView):
     model = Task
-    template_name = 'web_app/delete_task.html'
+    template_name = "web_app/delete_task.html"
 
     def get_success_url(self) -> HttpResponse:
-        return reverse_lazy('web_app:index')
+        return reverse_lazy("web_app:index")
 
 
 class ToggleTaskStatusView(generic.View):
     def get(self, request, *args, **kwargs) -> HttpResponse:
-        task = Task.objects.get(pk=kwargs['pk'])
+        task = Task.objects.get(pk=kwargs["pk"])
         task.is_done = not task.is_done
         task.save()
-        return redirect('web_app:index')
+        return redirect("web_app:index")
 
+
+class TagListView(generic.ListView):
+    model = Tag
+    template_name = "wep_app/tags.html"
+    context_object_name = "tags"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    template_name = "web_app/add_tag.html"
+    context_object_name = "tags"
+    fields = ["name"]
+
+    def get_success_url(self) -> HttpResponse:
+        return reverse_lazy("web_app:tags")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    template_name = "web_app/edit_tag.html"
+    fields = ["name"]
+
+    def get_success_url(self) -> HttpResponse:
+        return reverse_lazy("web_app:tags")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    template_name = "web_app/delete_tag.html"
+
+    def get_success_url(self) -> HttpResponse:
+        return reverse_lazy("web_app:tags")
